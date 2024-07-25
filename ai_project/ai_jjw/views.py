039a117c3a60a78aa3bfd2dba4026ab836d14be9
ds5pxi <ds5pxi@gmail.com>
2024-07-25 14:53:29 +0900

    jjw-변경사항

--- ai_project/ai_jjw/views.py
+++ ai_project/ai_jjw/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def learning(request):
+    return render(request, 'divorce/ai_jjw/learning.html');
--- ai_project/ai_project/settings.py
+++ ai_project/ai_project/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main', 'ai_khm',
+    'main', 'ai_khm', 'ai_jjw',
 ]
 
 MIDDLEWARE = [
