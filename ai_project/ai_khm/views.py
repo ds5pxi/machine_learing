72e0930c080cb47f77f695cec320d69a2f6e961a
damndest <groundbreaking@nate.com>
2024-07-26 13:22:26 +0900

    UI 샘플 화면 추가(실제 데이터 사용 시 변경 필요)

--- ai_project/ai_khm/views.py
+++ ai_project/ai_khm/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
+import pandas as pd
+import numpy as np
 
 # Create your views here.
 def learning(request):
-    return render(request, 'divorce/ai_khm/learning.html');
+    df_divorce = pd.read_csv('D:/machine_learing/ai_project/static/file/ai_data/ai_khm/divorce_data.csv', sep=';')
+
+    df_dvc_html = df_divorce.to_html(index=False, classes='table table-bordered')
+    
+    content = {
+        "df_dvc_html": df_dvc_html
+    }
+   
+    return render(request, 'divorce/ai_khm/learning.html', content)
+
+# 이혼 확률 데이터 전처리
+def divorce_data_preprocessing(df_divorce):
+    df_data = df_divorce.drop(['Divorce'], axis=1)
+    df_target = df_divorce['Divorce']
+     
+    print('데이터: ', df_data)
+    print('타겟: ', df_target.tail())
+
+    return df_data, df_target
