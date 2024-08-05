e050a80cfecc531ee4f8930ba8c3bc3ea268b702
damndest <groundbreaking@nate.com>
2024-08-05 13:39:25 +0900

    main 음식 결과 url 수정

--- ai_project/main/urls.py
+++ ai_project/main/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.main),
     path('foods/main/survey', views.foods_survey, name='foods_survey'),
     path('foods_ml', views.foods_ml, name='foods_ml'),
-    path('foods_result', views.foods_result, name='foods_result'),
+    path('foods/main/result', views.foods_result, name='foods_result'),
 ]
